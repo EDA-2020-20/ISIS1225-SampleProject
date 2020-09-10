ed24da8d251715590031d287c08c50eed1b28e49
aromerob1 <69620599+aromerob1@users.noreply.github.com>
2020-09-10 00:45:52 -0500

    Update view.py

--- App/view.py
+++ App/view.py
@@ -29,7 +29,7 @@ assert config
 
 """
 La vista se encarga de la interacción con el usuario.
-Presenta el menu de opciones  y  por cada seleccion
+Presenta el menu de opciones y por cada seleccion
 hace la solicitud al controlador para ejecutar la
 operación seleccionada.
 """
@@ -37,11 +37,10 @@ operación seleccionada.
 # ___________________________________________________
 #  Ruta a los archivos
 # ___________________________________________________
+moviesDetails = "Data/SmallMoviesDetailsCleaned.csv"
+moviesCasting = "Data/MoviesCastingRaw-small.csv"
 
 
-booksfile = 'GoodReads/books-small.csv'
-tagsfile = 'GoodReads/tags.csv'
-booktagsfile = 'GoodReads/book_tags-small.csv'
 
 
 # ___________________________________________________
@@ -51,94 +50,58 @@ booktagsfile = 'GoodReads/book_tags-small.csv'
 # ___________________________________________________
 
 
-def printAuthorData(author):
-    """
-    Imprime los libros de un autor determinado
-    """
-    if author:
-        print('Autor encontrado: ' + author['name'])
-        print('Promedio: ' + str(author['average_rating']))
-        print('Total de libros: ' + str(lt.size(author['books'])))
-        iterator = it.newIterator(author['books'])
-        while it.hasNext(iterator):
-            book = it.next(iterator)
-            print('Titulo: ' + book['title'] + '  ISBN: ' + book['isbn'])
-    else:
-        print('No se encontro el autor')
-
-
-def printBooksbyTag(books):
-    """
-    Imprime los libros que han sido clasificados con
-    una etiqueta
-    """
-    print('Se encontraron: ' + str(lt.size(books)) + ' Libros')
-    iterator = it.newIterator(books)
-    while it.hasNext(iterator):
-        book = it.next(iterator)
-        print(book['title'])
-
-
-def printBooksbyYear(books):
-    """
-    Imprime los libros que han sido publicados en un
-    año
-    """
-    print('Se encontraron: ' + str(lt.size(books)) + ' Libros')
-    iterator = it.newIterator(books)
-    while it.hasNext(iterator):
-        book = it.next(iterator)
-        print(book['title'])
-
 
 # ___________________________________________________
 #  Menu principal
+def printMenu ():
+    print("Bienvenido")
+    print("1. Cargar Datos")
+    print("2. Descubrir productoras de cine")
+    print("3. Conocer a un director")
+    print("4. Conocer a un actor")
+    print("5. Entender un género cinematográfico")
+    print("6. Encontrar películas por país") 
+    print("0. Salir")
 # ___________________________________________________
 
-
-def printMenu():
-    print("Bienvenido")
-    print("1- Inicializar Catálogo")
-    print("2- Cargar información en el catálogo")
-    print("3- Consultar los libros de un año")
-    print("4- Consultar los libros de un autor")
-    print("5- Consultar los Libros por etiqueta")
-    print("0- Salir")
-
-
-"""
-Menu principal
-"""
 while True:
     printMenu()
     inputs = input('Seleccione una opción para continuar\n')
 
     if int(inputs[0]) == 1:
-        print("Inicializando Catálogo ....")
-        # cont es el controlador que se usará de acá en adelante
-        cont = controller.initCatalog()
+        print("Cargando datos ....")
+        details = controller.loadMoviesDetails(moviesDetails)
+        castings = controller.loadMoviesCasting(moviesCasting)
+        detailsSize = controller.detailsSize(details)
+        castingsSize = controller.castingsSize(castings)
+        print("Se cargaron: "+str(detailsSize)+" datos")
+        print("Se cargaron: "+str(castingsSize)+" datos")
+        print(lt.firstElement(details)["original_title"])
+        print(lt.firstElement(details)["release_date"])
+        print(lt.firstElement(details)["vote_average"])
+        print(lt.firstElement(details)["vote_count"])
+        print(lt.firstElement(details)["original_language"])
+        print(" ")
+        print(lt.lastElement(details)["title"])
+        print(lt.lastElement(details)["release_date"])
+        print(lt.lastElement(details)["vote_average"])
+        print(lt.lastElement(details)["vote_count"])
+        print(lt.lastElement(details)["original_language"])
 
     elif int(inputs[0]) == 2:
-        print("Cargando información de los archivos ....")
-        controller.loadData(cont, booksfile, tagsfile, booktagsfile)
-        print('Libros cargados: ' + str(controller.booksSize(cont)))
-        print('Autores cargados: ' + str(controller.authorsSize(cont)))
-        print('Géneros cargados: ' + str(controller.tagsSize(cont)))
+        print("Opción aún no disponible")
 
     elif int(inputs[0]) == 3:
-        number = input("Buscando libros del año?: ")
-        books = controller.getBooksYear(cont, int(number))
-        printBooksbyYear(books)
+        print("Opción aún no disponible")
 
     elif int(inputs[0]) == 4:
-        authorname = input("Nombre del autor a buscar: ")
-        authorinfo = controller.getBooksByAuthor(cont, authorname)
-        printAuthorData(authorinfo)
+        print("Opción aún no disponible")
 
     elif int(inputs[0]) == 5:
-        label = input("Etiqueta a buscar: ")
-        books = controller.getBooksByTag(cont, label)
-        printBooksbyTag(books)
+        print("Opción aún no disponible")
+    
+    elif int(inputs[0]) == 6:
+        print("Opción aún no disponible")
     else:
         sys.exit(0)
 sys.exit(0)
